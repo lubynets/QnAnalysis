bf0350bfb2be21e3a6e0f43db886d0c30567eb9f
Lubynets <lubinets95@gmail.com>
2021-02-10 15:08:13 +0100

    first working approximation (toymc)

--- src/QnAnalysisDiscriminator/main.cxx
+++ src/QnAnalysisDiscriminator/main.cxx
@@ -44,9 +44,9 @@ private:
 
 int main(int argc, char** argv)
 {
-  TString sgnlfilename="pfsqa.sgnl_12.root";
-  TString bckgrfilename="pfsqa.bckgr.root";
-  TString v1filename="v1graph.toymc.fmd.all.root";
+  TString sgnlfilename="/home/user/cbmdir/working/massfit/pfsqa.sgnl_12.root";
+  TString bckgrfilename="/home/user/cbmdir/working/massfit/pfsqa.bckgr.root";
+  TString v1filename="/home/user/cbmdir/working/massfit/v1graph.toymc.fmd.all.root";
   
   TFile* v1file = TFile::Open(v1filename, "read");
   TGraph* v1graph = (TGraph*)v1file -> Get("v1_invmass");
