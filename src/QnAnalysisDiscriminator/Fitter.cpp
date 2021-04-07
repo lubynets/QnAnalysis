0750c9b90e0b47cfdc73f7d89493799488964454
Lubynets <lubinets95@gmail.com>
2021-04-07 16:36:30 +0200

    generalize Npar, add background fit

--- src/QnAnalysisDiscriminator/Fitter.cpp
+++ src/QnAnalysisDiscriminator/Fitter.cpp
@@ -5,17 +5,28 @@
 
 void Fitter::Fit()
 {
+  const int Npar = 3;
+  
   const float graphleft = graph_v_ -> GetPointX(0);                     //TODO read this info from Qn::Axis
   const float graphright = graph_v_ -> GetPointX(graph_v_->GetN()-1);
 
   MyFunctor funct(shape_);
-  TF1* f = new TF1("f", funct, graphleft-0.01, graphright+0.01, 3);
+  TF1* f = new TF1("f", funct, graphleft-0.01, graphright+0.01, Npar);
      
   graph_v_ -> Fit("f");
   
-  for(int i=0; i<3; i++)
+//   TF1* f2 = (TF1*)f->Clone("f2");
+  TF1* f2 = new TF1("f2", "[0] + [1]*(x-1.11572)", graphleft-0.01, graphright+0.01);
+  
+  for(int i=0; i<Npar; i++)
   {
+    if(i!=0)
+      f2->SetParameter(i-1, f->GetParameter(i));
+    
     fit_params_.push_back(f->GetParameter(i));
     fit_params_errors_.push_back(f->GetParError(i));
   }
+  
+  f2 -> SetLineColor(kBlue);
+  graph_v_ -> GetListOfFunctions() -> Add(f2);
 }
